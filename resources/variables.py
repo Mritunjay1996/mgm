875d97f7e896d090d61b4ae5cb474b2e1cb054b8
Mritunjay1996 <62329307+Mritunjay1996@users.noreply.github.com>
2020-06-08 17:42:16 +0530

    Update variables.py

--- resources/variables.py
+++ resources/variables.py
@@ -4,7 +4,7 @@ randNumber = str(random.randrange(1000, 9999))
 randomNumber = str(random.randrange(10000, 99999))
 
 email = "automation@practicallogix.com"
-password = "~;Baxg2t"
+password = "JNpR4dWX:A3cn"
 new_List_name = "automation" + randNumber
 mailinator_email = "automation" + randNumber + "@mailinator.com"
 mailinator_2email = "automation" + randomNumber + "@mailinator.com"
